d276cb3c9a7d640dff4cef76c3c6473e12dcc4e8
RobertTorres17 <49485666+RobertTorres17@users.noreply.github.com>
2019-04-10 09:42:07 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+// Declare a variable for the customer name
+     var customername = ("Eric");
+    $(".name").append(customername);
+// Give it a value
+
+
+// Select all elements with class "name", and use the .append() action to add the variable to elements with class "name"
+
+
+// Change the variable to hold a new value
